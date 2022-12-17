ae3377f3089127858817e09990ea44e5b85b46ee
jyzeng77 <jerryzeng59@gmail.com>
2022-12-16 21:52:36 -0500

    Top part

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,17 @@
+<script>
+
+    import { bubble, bubble, bubble } from "svelte/internal";
+
+</script>
 <!DOCTYPE html>
 
 <head>
     <title>Clide | Home</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+
 </head>
 
-<div class='navbar navbar-nav'> 
-    <div class='container-fluid'>
-        <a class='navbar-brand mb-0 h1' href='/'>Clide</a>
-    </div>
-</div>
+<nav> 
+    <span>Clide</span>
+</nav>
 
 
