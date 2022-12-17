06b090f4ca30a019eae81ac50386a839493f72b5
yohello <yohello@LAPTOP-49S9G7RL.localdomain>
2022-12-17 10:11:32 -0500

    File upload button

--- frontend/src/routes/join/+page.svelte
+++ frontend/src/routes/join/+page.svelte
@@ -6,6 +6,8 @@
   let link =
     "https://raw.githubusercontent.com/mozilla/pdf.js/ba2edeae/web/compressed.tracemonkey-pldi-09.pdf";
   let name = "";
+  let avatar, fileinput;
+
 
   function go() {
     if (code !== "" && name !== "") {
@@ -14,6 +16,15 @@
       goto(`/present/${code}`, { replaceState: false });
     }
   }
+
+  const onFileSelected = (file) => {
+    let image = file.target.files[0];
+    let reader = new FileReader();
+    reader.readAsDataURL(image);
+    reader.onload = (file) => {
+      avatar = file.target.result;
+    };
+  };
 </script>
 
 <h1>join room</h1>
@@ -25,5 +36,15 @@
 <p style="display:inline">Name:</p>
 <input bind:value={name} />
 <br />
+
+
+<div class="chan" on:click={()=>{fileinput.click();}}>Choose Image</div>
+<input
+  style="display:none"
+  type="file"
+  accept=".pdf"
+  on:change={(file) => onFileSelected(file)}
+  bind:this={fileinput}
+/>
 
 <button on:click={go}>go</button>
