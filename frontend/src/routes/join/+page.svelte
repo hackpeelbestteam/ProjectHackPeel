83b3c65a0a7ebe5997f6f0b6df426e392f2b52ec
Yohello1 <mp2702737@gmail.com>
2022-12-16 21:27:06 -0500

    Join code stuff

--- frontend/src/routes/join/+page.svelte
+++ frontend/src/routes/join/+page.svelte
@@ -1,5 +1,30 @@
 <h1>join room</h1>
 
+<script>
+
+function whenSubmitted()
+{
+  let form = document.getElementsByName('meetingJoin');
+
+   console.log(form)
+}
+
+
+const form  = document.getElementById('signup');
+
+form.addEventListener('meetingJoin', (event) => {
+    console.log("Hello world");
+});
+
+</script>
+
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
-</p>
+
+  <form name="meetingJoin">
+    <label>Join Code:</label>
+    <input name= "code" type="text"/>
+    <input type="submit" value="Join Meeting">
+  </form>
+
+<!-- </p> -->
