9a6f5cb7c65b82aded007ef80cfd758748d7588e
yohello <yohello@LAPTOP-49S9G7RL.localdomain>
2022-12-17 13:02:40 -0500

    Added working function

--- frontend/src/routes/room/[id]/+page.svelte
+++ frontend/src/routes/room/[id]/+page.svelte
@@ -1,7 +1,7 @@
 <script>
   import ioClient from "socket.io-client";
   import { goto } from "$app/navigation";
-  import { fade } from 'svelte/transition'
+  import { fade } from "svelte/transition";
   import { onMount } from "svelte";
   import QRCode from "./qr.svelte";
 
@@ -10,6 +10,9 @@
 
   //idfk
   let heart = false;
+  var heart_counter = 0;
+  var next_to_delete = 0;
+
 
   // io init
   const ENDPOINT = "http://5.78.50.153:3000";
@@ -26,14 +29,24 @@
   var url = "";
   var users = [];
 
+
   // Create & Delete heart
-  function heartMaker()
-  {
-    const div = document.createElement('div');
+  function heartMaker() {
+    const div = document.createElement("div");
 
-    div.className='row';
+    div.className = "row" + heart_counter;
+    heart_counter++;
 
+    div.innerHTML = `<img class="fade-in" src=../heart.png alt="heart">`;
+
+    document.getElementById("hearter").appendChild(div);
+
+    document.getElementById('hearter').removeChild(("row"+next_to_delete));
   }
+
+  function removeRow(input) {
+  document.getElementById('content').removeChild(input.parentNode);
+}
 
   // next slide
   function next() {
@@ -194,82 +207,80 @@
 </script>
 
 <body>
-{#if (url != null && url != "") || p != ""}
-  <button class="hostbutton" on:click={back}>back</button>
-  <button class="hostbutton" on:click={next}>next</button>
-  <button class="hostbutton" on:click={finish}>done</button>
-{:else}
-  <button class="hostbutton" on:click={exit}>❌ close room</button>
-  <p class="center">join with code</p>
-  <h1 class="roomcode center">{room}</h1>
-  <div class="centerchild">
-    <QRCode codeValue="http://5.78.50.153:5173/join" squareSize="150" />
-  </div>
-  <h4 class="center">select a presenter</h4>
-  <div class="centerchild">
-    <div class="seldiv">
-      {#each users as user}
-        {#if user != name}
-          <button class="userbutton" on:click={setuser(user)}>{user}</button>
-        {/if}
-      {/each}
+  {#if (url != null && url != "") || p != ""}
+    <button class="hostbutton" on:click={back}>back</button>
+    <button class="hostbutton" on:click={next}>next</button>
+    <button class="hostbutton" on:click={finish}>done</button>
+  {:else}
+    <button class="hostbutton" on:click={exit}>❌ close room</button>
+    <p class="center">join with code</p>
+    <h1 class="roomcode center">{room}</h1>
+    <div class="centerchild">
+      <QRCode codeValue="http://5.78.50.153:5173/join" squareSize="150" />
     </div>
+    <h4 class="center">select a presenter</h4>
+    <div class="centerchild">
+      <div class="seldiv">
+        {#each users as user}
+          {#if user != name}
+            <button class="userbutton" on:click={setuser(user)}>{user}</button>
+          {/if}
+        {/each}
+      </div>
+    </div>
+  {/if}
+
+  <div class="parent">
+    <div id="canvas" />
   </div>
-{/if}
 
-<div class="parent">
-  <div id="canvas" />
-</div>
+  {#if heart == false}
+    <!-- {heart = false} -->
+    <div id="hearter">
+      <!-- <h1 class="fade-in"></h1> -->
+    </div>
+  {/if}
 
-{#if heart == false}
-<!-- {heart = false} -->
-<div id="hearter">
-  <!-- <h1 class="fade-in"></h1> -->
-  <img class="fade-in" src=../heart.png alt="heart">
-</div>
-{/if}
+  <style>
+    .fade-in {
+      opacity: 0;
+      animation-name: fadeInOpacity;
+      animation-iteration-count: 1;
+      animation-timing-function: ease-in;
+      animation-duration: 2s;
+      height: 3%;
+      width: 3%;
 
-<style>
-  .fade-in {
-	opacity: 1;
-	animation-name: fadeInOpacity;
-	animation-iteration-count: 1;
-	animation-timing-function: ease-in;
-	animation-duration: 2s;
-  height: 3%;
-  width:  3%; 
+      transform: translate(0, 50px);
+      transition-duration: 500ms;
+    }
 
-  transform:translate(0, 50px);
-  transition-duration:500ms;
-  }
-
-@keyframes fadeInOpacity {
-	0% {
-		opacity: 1;
-    transform:translate(0,500px);
-	}
-	100% {
-		opacity: 0;
-        transform:translate(0,2px);
-
-	}
-}
-  canvas {
-    width: auto;
-    height: auto;
-    max-width: 90%;
-    max-height: 90%;
-  }
-  .parent {
-    z-index: -1;
-    display: flex;
-    position: fixed;
-    left: 0px;
-    top: 0px;
-    width: 100%;
-    height: 100%;
-    justify-content: center;
-    align-items: center;
-  }
-</style>
+    @keyframes fadeInOpacity {
+      0% {
+        opacity: 1;
+        transform: translate(0, 500px);
+      }
+      100% {
+        opacity: 0;
+        transform: translate(0, 2px);
+      }
+    }
+    canvas {
+      width: auto;
+      height: auto;
+      max-width: 90%;
+      max-height: 90%;
+    }
+    .parent {
+      z-index: -1;
+      display: flex;
+      position: fixed;
+      left: 0px;
+      top: 0px;
+      width: 100%;
+      height: 100%;
+      justify-content: center;
+      align-items: center;
+    }
+  </style>
 </body>
