8ee9d1b959d28861367e3868178cbc2706dc6117
Yohello1 <mp2702737@gmail.com>
2022-12-16 23:44:18 -0500

    Merged?

--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,17 +1,27 @@
 <!DOCTYPE html>
 
 <head>
-    <title>Clide | Home</title>
-    <!-- CSS only -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous">
-    <!-- JavaScript Bundle with Popper -->
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script>
+  <title>Clide | Home</title>
+  <!-- CSS only -->
+  <!-- <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-rbsA2VBKQhggwzxH7pPCaAqO46MgnOM80zW1RWuH61DGLwZJEdK2Kadq2F9CUG65" crossorigin="anonymous"> -->
+  <!-- JavaScript Bundle with Popper -->
+  <!-- <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous"></script> -->
 </head>
 
-<div class='navbar navbar-nav'> 
-    <div class='container-fluid'>
-        <a class='navbar-brand mb-0 h1' href='/'>Clide</a>
-    </div>
-</div>
+<script>
 
 
+</script>
+
+<body class="homepage_body">
+  <h1>Clide</h1>
+  <h4>The only place you'll ever have to vist when presenting something!</h4>
+
+  <span class="homepage_seporator" />
+  <span class="spacer_button_from_seporator" />
+
+
+  <button class="button_join" on:click={go}>Join</button>
+  <button class="button_join" on:click={go}>Host</button>
+
+</body>
