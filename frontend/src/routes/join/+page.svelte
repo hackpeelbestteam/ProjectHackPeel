ec9d2f877df7ae0968692bcb2d3a123dc9261a42
blobcode <lhelme123@gmail.com>
2022-12-17 12:22:52 -0500

    stuff

--- frontend/src/routes/join/+page.svelte
+++ frontend/src/routes/join/+page.svelte
@@ -4,31 +4,14 @@
   let code = "";
   let link = "";
   let name = "";
-  let avatar, fileinput;
 
   function go() {
     if (code !== "" && name !== "") {
       localStorage.setItem("usrName", name);
       localStorage.setItem("link", link);
-
-      if (fileinput.files.length == 0) {
-        console.log("Has file!");
-      } else {
-        console.log("No ");
-      }
-
       goto(`/present/${code}`, { replaceState: false });
     }
   }
-
-  const onFileSelected = (file) => {
-    let image = file.target.files[0];
-    let reader = new FileReader();
-    reader.readAsDataURL(image);
-    reader.onload = (file) => {
-      avatar = file.target.result;
-    };
-  };
 </script>
 
 <h1>clyde.</h1>
